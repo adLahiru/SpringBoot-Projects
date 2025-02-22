a5a7d5223e0dada32f82d02c47abb55f4e3050fc
Lahiru Dilshan <adlahiru65@gmail.com>
2025-02-23 01:09:37 +0530

    BookDTO initialized

---
+++ LibMgmt2025/src/main/java/lk/ijse/cmjd/LibMgmt2025/dto/BookDTO.java
@@ -0,0 +1,27 @@
+package lk.ijse.cmjd.LibMgmt2025.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookDTO implements Serializable {
+    private String bookId;
+    private String bookName;
+    private String author;
+    private String edition;
+    private String publisher;
+    private String isbn;
+    private Double price;
+    private Integer totalQty;
+    private Integer availableQty;
+    private LocalDate lastUpdateDate;
+    private Time lastUpdateTime;
+
+}
