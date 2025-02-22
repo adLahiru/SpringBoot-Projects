27749c3096bbd619adaf61aecc6cde646852aa28
Lahiru Dilshan <adlahiru65@gmail.com>
2025-02-23 00:46:15 +0530

    Add application.properties and test health test of BookController

--- LibMgmt2025/src/main/java/lk/ijse/cmjd/LibMgmt2025/controller/BookController.java
+++ LibMgmt2025/src/main/java/lk/ijse/cmjd/LibMgmt2025/controller/BookController.java
@@ -1,4 +1,16 @@
 package lk.ijse.cmjd.LibMgmt2025.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/book")
+
 public class BookController {
+
+    @GetMapping("/health")
+    public  String healthTest(){
+        return "Book Controller is running";
+    }
 }
